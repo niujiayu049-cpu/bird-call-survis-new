97f2e2ed239fce072c9fd1a008ae9270038fc86f
niujiayu049-cpu <niujiayu049@gmail.com>
2026-05-09 06:06:02 +0100

    Add files via upload

---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,115 @@
+﻿const generatedBibEntries = {
+    "arriaga2016vireo": {
+        "author": "Arriaga, Julio G. and Sanchez, Hector and Vallejo, Edgar E. and Hedley, Richard and Taylor, Charles E.",
+        "doi": "10.1016/j.neucom.2015.05.129",
+        "journal": "Neurocomputing",
+        "keywords": "individual identification, ensemble learning, bird calls, acoustic sequences",
+        "pages": "966--979",
+        "title": "Identification of Cassin's Vireo ({Vireo cassinii}) Individuals from Their Acoustic Sequences Using an Ensemble of Learners",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.neucom.2015.05.129",
+        "volume": "175",
+        "year": "2016"
+    },
+    "cole2022occupancy": {
+        "author": "Cole, J. S. and Michel, N. L. and Emerson, S. A. and Siegel, R. B.",
+        "doi": "10.1093/ornithapp/duac003",
+        "journal": "Ornithological Applications",
+        "keywords": "BirdNET, manual annotation, occupancy models, long-duration recordings",
+        "pages": "duac003",
+        "title": "Automated Bird Sound Classifications of Long-Duration Recordings Produce Occupancy Model Outputs Similar to Manually Annotated Data",
+        "type": "article",
+        "url": "https://doi.org/10.1093/ornithapp/duac003",
+        "volume": "124",
+        "year": "2022"
+    },
+    "kahl2021birdnet": {
+        "author": "Kahl, Stefan and Wood, Connor M. and Eibl, Maximilian and Klinck, Holger",
+        "doi": "10.1016/j.ecoinf.2021.101236",
+        "journal": "Ecological Informatics",
+        "keywords": "BirdNET, deep learning, bird sound recognition, avian diversity monitoring",
+        "pages": "101236",
+        "title": "{BirdNET}: A Deep Learning Solution for Avian Diversity Monitoring",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.ecoinf.2021.101236",
+        "volume": "61",
+        "year": "2021"
+    },
+    "lostanlen2018pcen": {
+        "author": "Lostanlen, Vincent and Salamon, Justin and Cartwright, Mark and McFee, Brian and Farnsworth, Andrew and Kelling, Steve and Bello, Juan Pablo",
+        "doi": "10.1109/LSP.2018.2878620",
+        "journal": "IEEE Signal Processing Letters",
+        "keywords": "PCEN, preprocessing, spectrogram, noise, audio classification",
+        "title": "Per-Channel Energy Normalization: Why and How",
+        "type": "article",
+        "url": "https://doi.org/10.1109/LSP.2018.2878620",
+        "year": "2018"
+    },
+    "perezgranados2025birdnet": {
+        "author": "P{\\'e}rez-Granados, C. and others",
+        "doi": "10.21203/rs.3.rs-6633549/v1",
+        "journal": "Research Square",
+        "keywords": "BirdNET settings, passive acoustic monitoring, optimisation, bird surveys",
+        "title": "Optimisation of Passive Acoustic Bird Surveys: A Global Assessment of {BirdNET} Settings",
+        "type": "article",
+        "url": "https://doi.org/10.21203/rs.3.rs-6633549/v1",
+        "year": "2025"
+    },
+    "potamitis2014detection": {
+        "author": "Potamitis, Ilyas and Ntalampiras, Stavros and Jahn, Olaf and Riede, Klaus",
+        "doi": "10.1016/j.apacoust.2014.01.001",
+        "journal": "Applied Acoustics",
+        "keywords": "bird sound detection, field recording, acoustic monitoring, long recordings",
+        "pages": "1--9",
+        "title": "Automatic Bird Sound Detection in Long Real-Field Recordings: Applications and Tools",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.apacoust.2014.01.001",
+        "volume": "80",
+        "year": "2014"
+    },
+    "stowell2016identity": {
+        "author": "Stowell, Dan and Morfi, Veronica and Gill, Lisa F.",
+        "booktitle": "Proceedings of INTERSPEECH 2016",
+        "doi": "10.48550/arXiv.1603.07236",
+        "keywords": "individual identification, songbirds, metric learning, acoustic features, bird calls",
+        "pages": "2607--2611",
+        "title": "Individual Identity in Songbirds: Signal Representations and Metric Learning for Locating the Information in Complex Corvid Calls",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.48550/arXiv.1603.07236",
+        "year": "2016"
+    },
+    "stowell2022bioacoustics": {
+        "author": "Stowell, Dan",
+        "doi": "10.7717/peerj.13152",
+        "journal": "PeerJ",
+        "keywords": "survey, deep learning, computational bioacoustics, passive acoustic monitoring",
+        "pages": "e13152",
+        "title": "Computational Bioacoustics with Deep Learning: A Review and Roadmap",
+        "type": "article",
+        "url": "https://doi.org/10.7717/peerj.13152",
+        "volume": "10",
+        "year": "2022"
+    },
+    "sun2023manx": {
+        "author": "Sun, Y. and others",
+        "doi": "10.1111/jav.03170",
+        "journal": "Journal of Avian Biology",
+        "keywords": "seabird, individual signature, acoustic features, bird calls",
+        "pages": "e03170",
+        "title": "Calls of Manx Shearwater {Puffinus puffinus} Contain Individual Signatures",
+        "type": "article",
+        "url": "https://doi.org/10.1111/jav.03170",
+        "volume": "2023",
+        "year": "2023"
+    },
+    "tseng2025thresholds": {
+        "author": "Tseng, S. and Hodder, D. P. and Otter, K. A.",
+        "doi": "10.1007/s10336-025-02260-w",
+        "journal": "Journal of Ornithology",
+        "keywords": "BirdNET, confidence threshold, precision, passive acoustic monitoring",
+        "title": "Setting {BirdNET} Confidence Thresholds: Species-Specific vs. Universal Approaches",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s10336-025-02260-w",
+        "year": "2025"
+    }
+};
